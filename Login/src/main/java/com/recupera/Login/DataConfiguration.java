d50d308974bca51f452dd31d7ffdf1ddadf817ac
--unset <--unset>
2025-06-27 19:00:33 -0300

    Model + Application.properties

--- Login/src/main/java/com/recupera/Login/DataConfiguration.java
+++ Login/src/main/java/com/recupera/Login/DataConfiguration.java
@@ -1,5 +1,29 @@
 package com.recupera.Login;
 
+import javax.sql.DataSource;
+
 public class DataConfiguration {
 
+//	@Bean
+//	public DataSource dataSource( ) {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//		dataSource.setUrl("jdbc:mysql://localhost:3306/login?useTimezone=true&serverTimezone=UTC");
+//		dataSource.setUsername("root");
+//		dataSource.setPassword("root");
+//		return dataSource();
+//	}
+//	
+//	@Bean
+//	public JpaVendorAdapter jpaVendorAdapter() {
+//		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//		adapter.setDatabase(Database.MYSQL);
+//		adapter.setShowSql(true);
+//		adapter.setGenerateDdl(true);
+//		adapter.setDatabasePlatform("org.hibernate.MariaDBDialect");
+//		adapter.setPrepareConnection(true);
+//		return adapter;
+//		
+//	}
+	
 }
